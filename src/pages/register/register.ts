8316bd59ccfddc61097e9bc2f1e4606a050f922f
Suneel Manyam <sunilhari9@users.noreply.github.com>
2017-08-11 15:00:37 +0530

    Registration page with new layout

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -10,137 +10,141 @@ import { AuthenticateProvider } from '../../providers/authenticate/authenticate'
  */
 @IonicPage()
 @Component({
-  selector: 'page-register',
-  templateUrl: 'register.html',
+    selector: 'page-register',
+    templateUrl: 'register.html',
 })
 export class RegisterPage {
-  
-  loading:Loading;
-  signUpSuccess = false;
-  signUpForm:FormGroup;
-  signUpAttempt:boolean=false;
-  signUpData = {
+
+    loading:Loading;
+signUpSuccess = false;
+signUpForm:FormGroup;
+signUpAttempt:boolean=false;
+signUpData = {
     contactNumber: "",
     emailAddress: "",
     firstName: "",
     lastName: "",
     loginPassword: "",
-    middleName: ""
-  };
- 
-  constructor(
+    middleName: "",
+    dob:"",
+    gender:""
+};
+
+constructor(
     private nav: NavController, 
     public navParams: NavParams, 
     private auth: AuthenticateProvider, 
     private alertCtrl: AlertController, 
     public formBuilder: FormBuilder,
     private loadingCtrl: LoadingController) {
-      this.signUpForm = this.formBuilder.group({
+    this.signUpForm = this.formBuilder.group({
         firstname: ['',Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z]+'), Validators.required]),''],
         lastname: ['',Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z]+'), Validators.required]),''],
         mobile: ['',Validators.compose([Validators.maxLength(10), Validators.pattern('[0-9]+'), Validators.required]),''],
         email: ['',Validators.compose([Validators.maxLength(30), Validators.pattern('^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$'), Validators.required]),''],
-        password: ['',Validators.compose([Validators.required]),'']
-      });    
-  }
- 
+        password: ['',Validators.compose([Validators.required]),''],
+        gender: ['',Validators.compose([Validators.required]),''],
+        dob: ['',Validators.compose([Validators.required]),'']
+    });    
+}
 
-  ionViewDidLoad() {
+
+ionViewDidLoad() {
     console.log('ionViewDidLoad RegisterPage');
-  }
+}
 
-  singUpSubmit() {
+singUpSubmit() {
     this.signUpAttempt = true;
     if(this.signUpForm.valid){
-      this.showLoading();
-      console.log('singUpSubmit:req:'+this.signUpData);
-      this.auth.registerUser(this.signUpData).subscribe(success => {
-        if((success.statusCode !== undefined)&&(success.statusCode == 0)) {
-          this.showOtpPoup(success);
-        } else {
-          this.showPopup("Error", success.statusMessage);
-        }
-      },
-      error => {
-        this.showPopup("Error", error);
-      });
+        this.showLoading();
+        console.log('singUpSubmit:req:'+this.signUpData);
+        this.auth.registerUser(this.signUpData).subscribe(success => {
+            if((success.statusCode !== undefined)&&(success.statusCode == 0)) {
+                this.showOtpPoup(success);
+            } else {
+                this.showPopup("Error", success.statusMessage);
+            }
+        },
+                                                          error => {
+            this.showPopup("Error", error);
+        });
     }
-  }
+}
 
-  showLoading() {
+showLoading() {
     this.loading = this.loadingCtrl.create({
-      content: 'Please wait...',
-      dismissOnPageChange: true
+        content: 'Please wait...',
+        dismissOnPageChange: true
     });
     this.loading.present();
-  }
-  
-  showOtpPoup(inputData){
+}
+
+showOtpPoup(inputData){
     if(this.loading !== undefined){
-      this.loading.dismiss();
+        this.loading.dismiss();
     }
     let alert = this.alertCtrl.create({
-      message: 'Please enter OTP:'+inputData.otp+' sent to your Mobile Number:'+inputData.contactNumber,
-      inputs: [
-        {
-          name: 'OTP',
-          placeholder: 'OTP'
-        },
-      ],
-      buttons: [
-        {
-          text: 'ReSend OTP?',
-          handler: data => {
-            console.log('ReSend clicked');
-          }
-        },
-        {
-          text: 'Done',
-          handler: data => {
-            this.showLoading();
-            inputData.otp = data;
-            this.authenticateUser(inputData);
-          }
-        }
-      ]
-    });
-    alert.present();
-  }
-
-  authenticateUser(inputData){
-    this.auth.authenticateUser(inputData).subscribe(success => {
-    if((success.statusCode !== undefined)&&(success.statusCode == 0)) {
-      this.auth.setCurrentUser({
-        statusMessage:success.statusMessage,
-        result:inputData,
-        jwtToken:success.jwtToken
-      });
-      this.nav.setRoot('HomePage');
-    } else {
-        this.showPopup("Error", success.statusMessage);
-      }
-    },
-    error => {
-      this.showPopup("Error", error);
-    });
-  }
-
-  showPopup(title, text) {
-    let alert = this.alertCtrl.create({
-      title: title,
-      subTitle: text,
-      buttons: [
-        {
-          text: 'OK',
-          handler: data => {
-            if (this.signUpSuccess) {
-              this.nav.popToRoot();
+        message: 'Please enter OTP:'+inputData.otp+' sent to your Mobile Number:'+inputData.contactNumber,
+        inputs: [
+            {
+                name: 'OTP',
+                placeholder: 'OTP'
+            },
+        ],
+        buttons: [
+            {
+                text: 'ReSend OTP?',
+                handler: data => {
+                    console.log('ReSend clicked');
+                }
+            },
+            {
+                text: 'Done',
+                handler: data => {
+                    this.showLoading();
+                    inputData.otp = data;
+                    this.authenticateUser(inputData);
+                }
             }
-          }
-        }
-      ]
+        ]
     });
     alert.present();
-  }
+}
+
+authenticateUser(inputData){
+    this.auth.authenticateUser(inputData).subscribe(success => {
+        if((success.statusCode !== undefined)&&(success.statusCode == 0)) {
+            this.auth.setCurrentUser({
+                statusMessage:success.statusMessage,
+                result:inputData,
+                jwtToken:success.jwtToken
+            });
+            this.nav.setRoot('HomePage');
+        } else {
+            this.showPopup("Error", success.statusMessage);
+        }
+    },
+                                                    error => {
+        this.showPopup("Error", error);
+    });
+}
+
+showPopup(title, text) {
+    let alert = this.alertCtrl.create({
+        title: title,
+        subTitle: text,
+        buttons: [
+            {
+                text: 'OK',
+                handler: data => {
+                    if (this.signUpSuccess) {
+                        this.nav.popToRoot();
+                    }
+                }
+            }
+        ]
+    });
+    alert.present();
+}
 
 }
