5614df1cf13f15d1a376196bafe07857c74c3c53
sudharshan <sudharshan.ui@gmail.com>
2017-09-06 23:53:49 +0530

    verify OTP screen

--- src/pages/verify-number/verify-number.ts
+++ src/pages/verify-number/verify-number.ts
@@ -14,9 +14,12 @@ import { AuthenticateProvider } from '../../providers/authenticate/authenticate'
   templateUrl: 'verify-number.html',
 })
 export class VerifyNumberPage {
-  public inputData:any;
+  public inputData:any={};
   public MobileNumber:any;
-  public showOtpPoup:any = "false";
+  public showOtpPoup:any = "true";
+  public HiddenMobNum:any = '';
+  public OTPresponse:any={};
+  items = ['','','','','',''];
   constructor(public navCtrl: NavController, public navParams: NavParams, private authProvider: AuthenticateProvider) {
     this.inputData = this.navParams.get("inputData");  
 
@@ -24,6 +27,11 @@ export class VerifyNumberPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad VerifyNumberPage');
+    //Test Data.
+    // this.HideMobCharacter('9711223344');
+    // this.OTPresponse.jwtToken = "1234567890";
+    // this.OTPresponse.result = {};
+    // this.OTPresponse.result.userId = "1234567";
   }
 
   GenerateOTP(){
@@ -32,10 +40,49 @@ export class VerifyNumberPage {
     this.authProvider.login(this.inputData).subscribe(success => {
     	if((success.status !== undefined)&&(success.status == '0009')) {
                 this.showOtpPoup = "true";
+                this.OTPresponse = success;
+                this.HideMobCharacter(this.MobileNumber);
             }else {
                 alert(success.status);
             }
     });
   }
 
+  SubmitOTP(){
+  	this.inputData = {};
+  	var OTP='';
+  	for(var item in this.items){
+  		OTP = OTP + this.items[item];
+  	}
+  	this.inputData.action ='OTP';
+    this.inputData.otp = parseInt(OTP);
+    this.inputData.token = this.OTPresponse.jwtToken;
+    this.inputData.userId = this.OTPresponse.result.userId;
+    this.authProvider.login(this.inputData).subscribe(success => {
+    	if((success.status !== undefined)&&(success.status == '0001')) {
+            this.authProvider.setCurrentUser(success);
+            this.authProvider.setUserData(success);
+            this.navCtrl.setRoot('HomePage');
+        }else{
+        	alert(success.status);
+        }
+    });
+  }
+
+
+  next(el) {
+    el.setFocus();
+  }
+  HideMobCharacter(MobileNumber){
+  	var i;
+  	MobileNumber = MobileNumber.split('');
+  	for(i=0; i<10;i++){
+	    for(i=0; i<10;i++){
+		    if(i==0||i==8||i==9)
+				this.HiddenMobNum= this.HiddenMobNum+MobileNumber[i];
+			else
+				this.HiddenMobNum= this.HiddenMobNum+'x';
+		}
+	}
+  }
 }
