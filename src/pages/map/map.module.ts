002cf3c8ca3bca073eb861790d2993bef44afe79
sudharshan <sudharshan.ui@gmail.com>
2017-09-22 23:42:13 +0530

    Removing unwanted declartions in map and hotelslider pages

--- src/pages/map/map.module.ts
+++ src/pages/map/map.module.ts
@@ -1,16 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-import { MapPage } from '../pages';
 
 @NgModule({
-  declarations: [
-    MapPage
-  ],
-  imports: [
-    IonicPageModule.forChild(MapPage),
-  ],
-  exports: [
-    MapPage
-  ]
 })
 export class MapPageModule {}
