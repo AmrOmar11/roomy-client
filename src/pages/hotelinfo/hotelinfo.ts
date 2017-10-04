ee5347a774c506b217c5466501376b65c6f5b078
Ramakrishna Gunupuru <rama1234.g@gmail.com>
2017-10-04 11:27:58 +0530

    latest icon

--- src/pages/hotelinfo/hotelinfo.ts
+++ src/pages/hotelinfo/hotelinfo.ts
@@ -13,90 +13,90 @@ import { AuthenticateProvider } from '../../providers/authenticate/authenticate'
     templateUrl: 'hotelinfo.html',
 })
 export class HotelinfoPage {
-  minDuration:any;
-  stayDuration:any;
-  stayDurationInput:any;
-  fareAmount:any;
-  hotelInfo:any;
-  userInfo:any;
-  hour :any = 1;
-  min :any = 0;
-  page1: any = 'DurationPage';
-  page2: any = 'AmenitiesPage';
-  page3: any = 'HotelPloicyPage';
+    minDuration:any;
+    stayDuration:any;
+    stayDurationInput:any;
+    fareAmount:any;
+    hotelInfo:any;
+    userInfo:any;
+    hour :any = 1;
+    min :any = 0;
+    page1: any = 'DurationPage';
+    page2: any = 'AmenitiesPage';
+    page3: any = 'HotelPloicyPage';
 
-constructor(public navCtrl: NavController, public navParams: NavParams,public alertCtrl: AlertController,private authProvider: AuthenticateProvider) {
-    this.hotelInfo = this.navParams.get("hotelInfo");
-    if(this.hotelInfo.minmumDurationPin <10){
-        this.stayDuration = "0"+this.hotelInfo.minmumDurationPin+":00";
-    }else{
-        this.stayDuration = this.hotelInfo.minmumDurationPin+":00";
-    }
-    this.minDuration = this.stayDuration;
-    this.userInfo = this.authProvider.getUserInfo();
-    this.calculatefare();
-}
+  constructor(public navCtrl: NavController, public navParams: NavParams,public alertCtrl: AlertController,private authProvider: AuthenticateProvider) {
+      this.hotelInfo = this.navParams.get("hotelInfo");
+      if(this.hotelInfo.minmumDurationPin <10){
+          this.stayDuration = "0"+this.hotelInfo.minmumDurationPin+":00";
+      }else{
+          this.stayDuration = this.hotelInfo.minmumDurationPin+":00";
+      }
+      this.minDuration = this.stayDuration;
+      this.userInfo = this.authProvider.getUserInfo();
+      this.calculatefare();
+  }
 
-calculatefare(){
-    this.stayDurationInput = this.stayDuration;
-    this.stayDurationInput = this.stayDurationInput.toString();
-    this.stayDurationInput = this.stayDurationInput.split(":")
-    this.stayDurationInput = ((parseInt(this.stayDurationInput[0])*60)+ parseInt(this.stayDurationInput[1]));
-    //fareAmount = Hotelcharge + Tax;
-    this.fareAmount = ((this.stayDurationInput*(this.hotelInfo.pricePerMin)) + ((this.stayDurationInput*(this.hotelInfo.pricePerMin))*(parseFloat(this.hotelInfo.tax)/100))).toFixed(2);
-}
+  calculatefare(){
+      this.stayDurationInput = this.stayDuration;
+      this.stayDurationInput = this.stayDurationInput.toString();
+      this.stayDurationInput = this.stayDurationInput.split(":")
+      this.stayDurationInput = ((parseInt(this.stayDurationInput[0])*60)+ parseInt(this.stayDurationInput[1]));
+      //fareAmount = Hotelcharge + Tax;
+      this.fareAmount = ((this.stayDurationInput*(this.hotelInfo.pricePerMin)) + ((this.stayDurationInput*(this.hotelInfo.pricePerMin))*(parseFloat(this.hotelInfo.tax)/100))).toFixed(2);
+  }
 
-dismiss(){
+  dismiss(){
     this.navCtrl.pop();
-}
+  }
 
-reserveNow(){
+  reserveNow(){
     let confirm = this.alertCtrl.create({
         message: 'Booking For '+this.userInfo.first_Name+" ?",
         buttons: [
             {
                 text: 'Other',
                 handler: () => {
-                    //console.log(this.authProvider);
-                    this.navCtrl.push('BookingPage',{for:"other",
-                          hotelInfo:{
-                            hotelName:this.hotelInfo.hotelName,
-                            stayDuration:this.stayDuration
-                          }
-                        }
-                      );
+                  //console.log(this.authProvider);
+                  this.navCtrl.push('BookingPage',{for:"other",
+                      hotelInfo:{
+                        hotelName:this.hotelInfo.hotelName,
+                        stayDuration:this.stayDuration
+                      }
+                    }
+                  );
                 }
             },
             {
                 text: 'Yes',
                 handler: () => {
-                    this.navCtrl.push('BookingPage',{for:"me",
-                          hotelInfo:{
-                            hotelName:this.hotelInfo.hotelName,
-                            stayDuration:this.stayDuration
-                          }
-                        }
-                      );
+                  this.navCtrl.push('BookingPage',{for:"me",
+                      hotelInfo:{
+                        hotelName:this.hotelInfo.hotelName,
+                        stayDuration:this.stayDuration
+                      }
+                    }
+                  );
                 }
             }
         ]
     });
     confirm.present();
-}
+  }
 
-    getRating(rating){
-      let ratingString = '';
-      rating = parseInt(rating);
-      if(rating == undefined || rating <= 3)
-      {
-        ratingString = "three";
-      }else if(rating == 5)
-      {
-        ratingString = "five";
-      }else if(rating == 7)
-      {
-        ratingString = "seven";
-      }
-      return ratingString;
+  getRating(rating){
+    let ratingString = '';
+    rating = parseInt(rating);
+    if(rating == undefined || rating <= 3)
+    {
+      ratingString = "three";
+    }else if(rating == 5)
+    {
+      ratingString = "five";
+    }else if(rating == 7)
+    {
+      ratingString = "seven";
     }
+    return ratingString;
+  }
 }
