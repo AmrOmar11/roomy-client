e57a94629d90fbe3c9f4290e3e10e932820ed415
sudharshan <sudharshan.ui@gmail.com>
2017-10-17 18:58:42 +0530

    Test Call Hotel and Open Map Func

--- src/pages/booking-confirm/booking-confirm.ts
+++ src/pages/booking-confirm/booking-confirm.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
+import { Geolocation,PositionError } from '@ionic-native/geolocation';
 
 /**
  * Generated class for the BookingConfirmPage page.
@@ -7,6 +8,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * See http://ionicframework.com/docs/components/#navigation for more info
  * on Ionic pages and navigation.
  */
+declare var google:any;
 @IonicPage()
 @Component({
   selector: 'page-booking-confirm',
@@ -14,12 +16,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class BookingConfirmPage {
   public userInfo:any;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public location:any;
+  public hotelphonenumber: 8143509343;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public platform:Platform, public geolocation:Geolocation) {
   	this.userInfo = this.navParams.get("userInfo"); 
   }
 
   ionViewDidLoad() {
     //console.log('ionViewDidLoad BookingConfirmPage');
+    // this.location = new google.maps.LatLng("17.4474257", "78.3609022");
+     this.location = {latitude:"17.4474257",longitude:"78.3609022", name:"Hotel"}
   }
+
+ 
+  startExternalMap() {
+	  if (this.location.latitude) {
+	    this.platform.ready().then(() => {
+	      this.geolocation.getCurrentPosition().then((position) => {
+	        // ios
+	        if (this.platform.is('ios')) {
+	          window.open('maps://?q=' + this.location.name + '&saddr=' + position.coords.latitude + ',' + position.coords.longitude + '&daddr=' + this.location.latitude + ',' + this.location.longitude, '_system');
+	        };
+	        // android
+	        if (this.platform.is('android')) {
+	          window.open('geo://' + position.coords.latitude + ',' + position.coords.longitude + '?q=' + this.location.latitude + ',' + this.location.longitude + '(' + this.location.name + ')', '_system');
+	        }else{
+	        	console.log("Not android");
+	        };
+
+	      });
+	    });
+	  };
+	}
 
 }
